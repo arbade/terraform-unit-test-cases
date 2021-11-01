c4e0d626d2d4f3a35782e741f5b59be71e857f35
arbade <ardabthn@gmail.com>
2021-11-01 19:49:45 +0300

    added network configs & test cases

--- examples/variable.tf
+++ examples/variable.tf
@@ -1,6 +1,5 @@
-variable "region" {
-  description = "The AWS region to deploy to"
-  type        = string
+variable "aws_region" {
+  default = "eu-central-1"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -12,4 +11,25 @@ variable "table_name" {
   description = "The name to set for the dynamoDB table."
   type        = string
   default     = "terratest-example"
+}
+
+variable "main_vpc_cidr" {
+  description = "The CIDR of the main VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR of public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "The CIDR of the private subnet"
+  type        = string
+}
+
+variable "tag_name" {
+  description = "A name used to tag the resource"
+  type        = string
+  default     = "terraform-network-example"
 }
